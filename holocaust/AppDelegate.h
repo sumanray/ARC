61e98c7cafb2f4a831cc5564653f9c8bc896f8c0
Suman Ray <admin@Daniels-MacBook-Pro-2.local>
2013-11-13 12:30:03 -0500

    Initial Commit

---
+++ holocaust/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  holocaust
+//
+//  Created by Suman Ray on 11/13/13.
+//  Copyright (c) 2013 ATT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ holocaust/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  holocaust
+//
+//  Created by Suman Ray on 11/13/13.
+//  Copyright (c) 2013 ATT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
